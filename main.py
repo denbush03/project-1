bb750fa3ebb60097c7d31610e4e181cc5661a9dd
Denis <82325126+denbush03@users.noreply.github.com>
2024-04-18 15:31:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-import random
+from kivy.uix.textinput import TextInput
 
-class RandomNumberApp(App):
+class Interface(FloatLayout):
+    def display_information(self):
+        data=self.ids.textInput.text
+        self.ids.label.text=data
+class ProjectApp(App):
+    pass
 
-    def build(self):
-        self.layout = BoxLayout(orientation='vertical')
-        
-        self.label = Label(text="Random number will display here")
-        self.layout.add_widget(self.label)
-        
-        self.button = Button(text="Generate Random Number")
-        self.button.bind(on_press=self.generate_random_number)
-        self.layout.add_widget(self.button)
-        
-        return self.layout
-    
-    def generate_random_number(self, instance):
-        random_num = random.randint(1, 100)
-        self.label.text = f"Random Number: {random_num}"
-
-if __name__ == "__main__":
-    RandomNumberApp().run()
+ProjectApp().run()
